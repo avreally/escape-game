224a5f6427cf4f20b56375a53e987991e0244179
Valeria <waleria.shadrina@gmail.com>
2022-06-05 15:04:56 +0200

    Moved functionality from index.js to moving-grid.js, added this.magnitude, added a-text and custom font

--- components/moving-grid.js
+++ components/moving-grid.js
@@ -7,6 +7,8 @@ AFRAME.registerComponent("moving-grid", {
   },
 
   init: function () {
+    this.magnitude = 0;
+
     var el = this.el;
 
     var division = 20;
@@ -70,10 +72,51 @@ AFRAME.registerComponent("moving-grid", {
     });
 
     el.setObject3D("mesh", this.grid);
+
+    document
+      .querySelector(".button-connect")
+      .addEventListener("click", async () => {
+        const usbVendorId = 0x239a;
+        navigator.serial
+          .requestPort({ filters: [{ usbVendorId }] })
+          .then(async (port) => {
+            await port.open({ baudRate: 9600 });
+            console.log(port, port.readable);
+
+            while (port.readable) {
+              const reader = port.readable.getReader();
+              try {
+                while (true) {
+                  const { value, done } = await reader.read();
+                  if (done) {
+                    console.error("Reader has been canceled");
+                    break;
+                  }
+                  const view = new DataView(value.buffer, 0);
+                  const magnitude = view.getFloat32(0, true);
+
+                  if (value.length === 5) {
+                    // console.log(value.length, magnitude);
+                    this.magnitude = magnitude;
+                  }
+                }
+              } catch (error) {
+                console.error(error);
+              } finally {
+                reader.releaseLock();
+              }
+            }
+          })
+          .catch((err) => {
+            console.error("Port is not selected", err);
+          });
+      });
   },
 
   tick: function (time, timeDelta) {
     this.grid.material.uniforms.time.value = time;
     this.grid.material.uniforms.speed.value = time * 0.0000001 + 0.01;
+    // this.grid.material.uniforms.speed.value = this.magnitude * 0.0001;
+    // console.log(this.grid.material.uniforms.speed.value);
   },
 });
