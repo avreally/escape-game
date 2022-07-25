d3b03732428d898d6a2f60da957a19a21699fa1e
Valeria <waleria.shadrina@gmail.com>
2022-07-25 21:07:51 +0200

    Generating obstacles more often when speed increases

--- components/obstacles.js
+++ components/obstacles.js
@@ -1,8 +1,8 @@
 AFRAME.registerComponent("obstacles", {
   init: function () {
-    this.time = 0;
     this.speed = 0;
     this.elements = [];
+    this.delay = 2000;
 
     this.playerElement = document.querySelector("a-entity[player]");
 
@@ -26,64 +26,64 @@ AFRAME.registerComponent("obstacles", {
     const max = 18;
     const min = -18;
 
-    this.hasPlayerWon = false;
+    let timeoutID;
 
-    let intervalID;
-    if (this.hasPlayerWon === false) {
-      // Generating obstacles and bonuses
-      intervalID = setInterval(() => {
-        // Decide between obstacle or bonus, 0 - 3 = obstacle, 4 - 5 = bonus, 6 - 7 - letter
-        const randomType = Math.floor(Math.random() * 8);
+    const generateObstacles = () => {
+      // Decide between obstacle or bonus, 0 - 3 = obstacle, 4 - 5 = bonus, 6 - 7 - letter
+      const randomType = Math.floor(Math.random() * 8);
 
-        // Random x-coordinate
-        const x = Math.floor(Math.random() * (max - min) + min);
+      // Random x-coordinate
+      const x = Math.floor(Math.random() * (max - min) + min);
 
-        let element;
-        if (randomType < 4) {
-          const randomImgIndex = Math.floor(
-            Math.random() * obstaclesImages.length
-          );
-          element = document.createElement("a-box");
-          element.setAttribute("width", 10);
-          element.setAttribute("height", 10);
-          element.setAttribute("depth", 10);
-          element.setAttribute("src", obstaclesImages[randomImgIndex]);
-          element.setAttribute("class", "collidable obstacle");
-          element.object3D.position.set(x, 5, -150);
-        }
+      let element;
+      if (randomType < 4) {
+        const randomImgIndex = Math.floor(
+          Math.random() * obstaclesImages.length
+        );
+        element = document.createElement("a-box");
+        element.setAttribute("width", 10);
+        element.setAttribute("height", 10);
+        element.setAttribute("depth", 10);
+        element.setAttribute("src", obstaclesImages[randomImgIndex]);
+        element.setAttribute("class", "collidable obstacle");
+        element.object3D.position.set(x, 5, -150);
+      }
 
-        if (randomType > 3 && randomType < 6) {
-          element = document.createElement("a-octahedron");
-          element.setAttribute("radius", 3);
-          element.setAttribute("color", "#fbff12");
-          element.setAttribute("class", "collidable bonus");
-          element.object3D.position.set(x, 3, -150);
-        }
+      if (randomType > 3 && randomType < 6) {
+        element = document.createElement("a-octahedron");
+        element.setAttribute("radius", 3);
+        element.setAttribute("color", "#fbff12");
+        element.setAttribute("class", "collidable bonus");
+        element.object3D.position.set(x, 3, -150);
+      }
 
-        if (randomType > 5) {
-          const letterIndex = Math.floor(Math.random() * lettersImages.length);
-          element = document.createElement("a-sphere");
-          element.setAttribute("src", lettersImages[letterIndex]);
-          element.setAttribute("radius", 5);
-          element.setAttribute("segments-height", 53);
+      if (randomType > 5) {
+        const letterIndex = Math.floor(Math.random() * lettersImages.length);
+        element = document.createElement("a-sphere");
+        element.setAttribute("src", lettersImages[letterIndex]);
+        element.setAttribute("radius", 5);
+        element.setAttribute("segments-height", 53);
 
-          element.setAttribute(
-            "class",
-            `collidable letter ${lettersClass[letterIndex]}`
-          );
-          element.object3D.position.set(x, 5, -150);
-        }
+        element.setAttribute(
+          "class",
+          `collidable letter ${lettersClass[letterIndex]}`
+        );
+        element.object3D.position.set(x, 5, -150);
+      }
 
-        element.setAttribute("data-aabb-collider-dynamic", true);
+      element.setAttribute("data-aabb-collider-dynamic", true);
 
-        this.elements.push({ element });
-        this.el.appendChild(element);
-      }, 2000);
-    }
+      this.elements.push({ element });
+      this.el.appendChild(element);
+
+      timeoutID = setTimeout(() => generateObstacles(), this.delay);
+    };
+
+    // Generating obstacles and bonuses
+    timeoutID = setTimeout(() => generateObstacles(), 2000);
 
     // Removing obstacles after collision with player
     this.playerElement.addEventListener("hitstart", () => {
-      // console.log(this.elements);
       const collidedElement =
         this.playerElement.components["aabb-collider"].objectEls[0];
 
@@ -97,18 +97,14 @@ AFRAME.registerComponent("obstacles", {
     document
       .querySelector("a-entity[core]")
       .addEventListener("updateTimeState", (event) => {
-        const { time, speed } = event.detail;
-        this.time = time;
+        const { speed } = event.detail;
         this.speed = speed;
       });
 
     document
       .querySelector("a-entity[core]")
       .addEventListener("playerWon", () => {
-        this.hasPlayerWon = true;
-        // console.log("player won, removing obstacles");
-        clearInterval(intervalID);
-        // console.log("elements", this.elements);
+        clearInterval(timeoutID);
         this.elements.forEach((element) => {
           element.element.remove();
         });
@@ -125,5 +121,11 @@ AFRAME.registerComponent("obstacles", {
         this.el.removeChild(element);
       }
     });
+
+    if (this.delay < 800) {
+      this.delay = 800;
+    } else {
+      this.delay -= this.speed * 2;
+    }
   },
 });
